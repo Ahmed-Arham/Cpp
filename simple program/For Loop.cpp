2aec67885c0dd56e875db39434dd361c45c527b0
Ahmed-Arham <arhamahmed064@gmail.com>
2025-09-07 14:29:36 +0530

    Add files via upload

---
+++ simple program/For Loop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char grade = 'B';
+    switch(grade) {
+        case 'A': cout << "Excellent"; break;
+        case 'B': cout << "Good"; break;
+        case 'C': cout << "Average"; break;
+        default: cout << "Invalid grade";
+    }
+    return 0;
+}
